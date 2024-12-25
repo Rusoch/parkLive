28404cbef43c54fdb8778147e37a2f36b116735c
alsandre <gabeditebuli@gmail.com>
2024-12-25 21:35:12 +0400

    fix: additiona styling

--- src/app/components/InfoPopup.tsx
+++ src/app/components/InfoPopup.tsx
@@ -28,16 +28,16 @@ const InfoPopup: React.FC<TProps> = ({ isOpen, onClose, placeData }) => {
     const { address, totalSpace, freeSpace, rate, paymentType, opens, closes } = placeData;
     const paymentKeyword = paymentType[0] === "მხოლოდ ქეში" ? "cashOnly" : "";
     return (
-        <div className="bg-white dark:bg-[#0D0D0D] rounded-lg shadow-lg flex items-center flex-col px-[14px] text-[#15593A] dark:text-[#15593A] text-[20px]">
+        <div className="bg-white dark:bg-[#0D0D0D] rounded-t-lg shadow-lg flex items-center flex-col px-[14px] text-[#15593A] dark:text-[#15593A] text-[20px] fixed bottom-0 left-0 w-full">
             <span className="w-[20%] self-center h-1 rounded-lg bg-[#D9D9D9] dark:bg-[#333333] m-auto mt-[10px] mb-3"></span>
-            <div className="w-full py-4 flex justify-between">
+            <div className="w-full flex justify-between">
                 <span>{t(address)}</span>
                 <div className="flex gap-3">
                     <ShareIcon />
                     <CancelIcon onClick={onClose} />
                 </div>
             </div>
-            <ul className="flex flex-col w-full py-4 items-start gap-1">
+            <ul className="flex flex-col w-full mt-4 items-start gap-1 text-[14px]">
                 <li className="flex items-center gap-2 justify-center">
                     <PlacesIcon /> {`${freeSpace}/${totalSpace} ${t("place")}`}
                 </li>
@@ -55,12 +55,12 @@ const InfoPopup: React.FC<TProps> = ({ isOpen, onClose, placeData }) => {
                 <Button
                     onClick={() => console.log("მისვლა")}
                     label={t("directions")}
-                    className="rounded-[14px] w-[44%] py-2 border bg-[#15593A] text-white dark:text-[#0D0D0D] dark:bg-[#1A6E48]"
+                    className="rounded-[14px] w-[44%] py-2 border bg-[#15593A] border-[#15593A] dark:border-[#1A6E48] text-white dark:text-[#0D0D0D] dark:bg-[#1A6E48] text-[14px]"
                 />
                 <Button
                     onClick={() => console.log("შენახვა")}
                     label={t("save")}
-                    className="rounded-[14px] w-[44%] py-2 border border-[#15593A] text-[#15593A] dark:text-[#1A6E48] dark:border-[#1A6E48]"
+                    className="rounded-[14px] w-[44%] py-2 border border-[#15593A] text-[#15593A] dark:text-[#1A6E48] dark:border-[#1A6E48] text-[14px]"
                 />
             </div>
         </div>
