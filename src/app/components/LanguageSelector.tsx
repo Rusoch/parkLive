51780203079e88b8e4c4ef7910dd300bb0ea7ba8
lamia1283 <lamiamir361@gmail.com>
2025-02-15 23:07:06 +0400

    update icons

--- src/app/components/LanguageSelector.tsx
+++ src/app/components/LanguageSelector.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { GeoFlagIcon } from "../icons/GeoFlagIcon";
+import { EngFlagIcon } from "../icons/EngFlagIcon";
+
 type TProps = { className?: string; isVisible: boolean; onClick: () => void };
 
-export const LanguageSelector: React.FC<TProps> = ({ isVisible, onClick, className }) => {
+export const LanguageSelector: React.FC<TProps> = ({ onClick, className }) => {
   const { i18n } = useTranslation();
   const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
 
@@ -10,30 +13,19 @@ export const LanguageSelector: React.FC<TProps> = ({ isVisible, onClick, classNa
     setSelectedLanguage(language);
     i18n.changeLanguage(language);
   };
+  console.log("lami");
   return (
-    <>
-      {isVisible && (
-        <div
-          className={`absolute top-10 right-2 bg-[#F3F6FF] dark:bg-[#131313] border border-solid border-[#F3F6FF] rounded-2xl shadow-md p-0 transition-colors ${className ?? ""}`}
-        >
-          {["ka", "en"].map((language) => (
-            <div
-              key={language}
-              onClick={() => {
-                changeLanguage(language);
-                onClick();
-              }}
-              className={`w-[145px] h-[43px] cursor-pointer rounded-[16px] flex items-center justify-center text-black dark:text-white transition-colors ${
-                selectedLanguage === language
-                  ? "border border-[#15593A]"
-                  : "border border-transparent"
-              }`}
-            >
-              {language === "ka" ? "ქართული" : "English"}
-            </div>
-          ))}
-        </div>
-      )}
-    </>
+    <div className={`relative ${className ?? ""}`}>
+      <div
+        className="cursor-pointer rounded-[2px] overflow-hidden"
+        onClick={() => {
+          const newLanguage = selectedLanguage === "ka" ? "en" : "ka";
+          changeLanguage(newLanguage);
+          onClick();
+        }}
+      >
+        {selectedLanguage === "ka" ? <GeoFlagIcon /> : <EngFlagIcon />}
+      </div>
+    </div>
   );
 };
--- src/app/components/ThemeSwitcher.tsx
+++ src/app/components/ThemeSwitcher.tsx
@@ -1,10 +1,12 @@
-"use client";
 import { useEffect, useState } from "react";
-import { ContrastIcon } from "../icons/ContrastIcon";
+import { MoonIcon } from "../icons/MoonIcon";
+import { SunIcon } from "../icons/SunIcon";
 
 type TProps = { onClick: () => void };
+
 export const ThemeSwitcher: React.FC<TProps> = ({ onClick }) => {
   const [theme, setTheme] = useState<"light" | "dark">("light");
+
   useEffect(() => {
     const savedTheme = localStorage.getItem("user-theme") as "light" | "dark" | null;
     if (savedTheme) {
@@ -14,13 +16,21 @@ export const ThemeSwitcher: React.FC<TProps> = ({ onClick }) => {
       setTheme("light");
     }
   }, []);
+
   const handler = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     localStorage.setItem("user-theme", newTheme);
     setTheme(newTheme);
     onClick();
   };
+
   return (
-    <ContrastIcon onClick={handler} className="text-white dark:text-black transition-colors" />
+    <div onClick={handler} className="cursor-pointer">
+      {theme === "light" ? (
+        <SunIcon className="text-black dark:text-white transition-colors" />
+      ) : (
+        <MoonIcon className="text-black dark:text-white transition-colors" />
+      )}
+    </div>
   );
 };
