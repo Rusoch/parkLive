e4e2d6ed23269411f42fb486599860b9d21990e8
Alsandre <86976625+Alsandre@users.noreply.github.com>
2025-04-06 11:05:48 +0400

    fix: warnning message gets overlapped

--- src/app/components/WarningMessage.tsx
+++ src/app/components/WarningMessage.tsx
@@ -6,7 +6,7 @@ interface WarningMessageProps {
 
 export const WarningMessage: React.FC<WarningMessageProps> = ({ message, type }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center backdrop-blur-sm">
+    <div className="fixed inset-0 flex items-center justify-center backdrop-blur-sm z-[60]">
       <div
         className={`w-[91%] h-[11%] bg-bg-primary text-green-dark text-[14px] font-semibold flex items-center justify-center gap-2 rounded-lg border-2 text-center 
         ${type === "success" ? "border-green-light" : "border-border-error"}`}
